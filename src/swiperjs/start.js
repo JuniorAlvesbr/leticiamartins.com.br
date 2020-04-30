a15b5cb0931005106bc1a2c778c29af66a309da9
Junior Alves <jrnalves@gmail.com>
2020-04-30 12:08:24 -0300

    add swiper autoplay

--- src/swiperjs/start.js
+++ src/swiperjs/start.js
@@ -1,5 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
   cssMode: true,
+  autoplay: {
+    delay: 4000
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
