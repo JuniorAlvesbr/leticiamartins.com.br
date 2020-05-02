f7575541cbc15d5d26efd706db18d9c424c5975b
Junior Alves <jrnalves@gmail.com>
2020-05-02 11:53:17 -0300

    :sparkles: create header-bar fixed

---
+++ src/header-bar-fixed.js
@@ -0,0 +1,11 @@
+const $headerBarFixed = document.querySelector('.header-bar-fixed');
+const $headerBar = document.querySelector('.header-bar');
+
+window.addEventListener('scroll', () => {
+    let scroll = this.scrollY;
+    if(scroll > 90) {
+        $headerBarFixed.classList.add('-active');
+    } else {
+        $headerBarFixed.classList.remove('-active');
+    }
+})
