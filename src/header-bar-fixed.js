6c4791575c8e4a85929dcc9226ffd4c26779c55b
Junior Alves <jrnalves@gmail.com>
2020-05-06 10:27:45 -0300

    change header-bar size on the scroll

--- src/header-bar-fixed.js
+++ src/header-bar-fixed.js
@@ -5,7 +5,9 @@ window.addEventListener('scroll', () => {
     let scroll = this.scrollY;
     if(scroll > 90) {
         $headerBarFixed.classList.add('-active');
+        $headerBar.classList.add('-fixed')
     } else {
         $headerBarFixed.classList.remove('-active');
+        $headerBar.classList.remove('-fixed');
     }
 })
