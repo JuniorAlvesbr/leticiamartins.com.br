3049db023aa2a752568ba8f865689f26aea8ce70
Junior Alves <jrnalves@gmail.com>
2020-06-08 14:31:29 -0300

    Create swiper carousel in last projects

--- src/swiperjs/start.js
+++ src/swiperjs/start.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper('.swiper-container', {
+const swiper1 = new Swiper('.swiper1', {
   cssMode: false,
   autoplay: {
     delay: 4000
@@ -8,8 +8,29 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination1',
   },
   // mousewheel: true,
+  keyboard: true,
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: '.swiper-pagination2',
+  },
   keyboard: true,
 });
