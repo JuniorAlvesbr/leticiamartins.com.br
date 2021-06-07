fbccff1b1ba725761d48dea531d4b528771b793d
Junior Alves <jrnalves@gmail.com>
2021-06-07 16:37:35 -0300

    remove modal and video section

--- src/modal.js
+++
@@ -1,6 +0,0 @@
-function activeModal() {
-  const $modal = document.querySelector('[data-modal="modal"]');
-  const $modalSmall = document.querySelector('[data-modal="modal-small"]');
-  $modal.classList.toggle("-active");
-  $modalSmall.classList.toggle("-active");
-}
